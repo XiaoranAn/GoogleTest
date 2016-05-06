7596a20858d2192ca0f637aab5c4b8f9cd07027e
ANXIAORAN <sherryann87827@gmail.com>
2016-05-06 00:03:40 -0700

    Add test case

--- src/main/java/com/seleniumtest/App.java
+++ src/main/java/com/seleniumtest/App.java
@@ -3,6 +3,7 @@ package com.seleniumtest;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
+
 /**
  * Hello world!
  *
@@ -14,7 +15,19 @@ public class App
     public void setUp() {
 		pObj = new PageObject();
     }
- 
+	
+	@Test
+	public void validateElements() {
+		assertNotNull(pObj.getInputBox());
+		assertNotNull(pObj.getSubmitBtn());
+		assertNotNull(pObj.getLuckyBtn());
+		assertEquals(pObj.getSubmitBtn().getAttribute("value"),"Google Search");
+		assertEquals(pObj.getGmailLink().getAttribute("href"), "https://mail.google.com/mail/?tab=wm");
+	}
+	
+    /**
+     * Test search empty string
+     */
     @Test
     public void testEmptyStringNotChangeUrl() {
     	String curUrl = pObj.getCurrentUrl();
@@ -22,12 +35,34 @@ public class App
     	assertEquals(pObj.getCurrentUrl(), curUrl);
     }
  
+    /**
+     * Test search text
+     */
     @Test
     public void testSearchText() {
         String searchStr = "test";
         pObj.search(searchStr);
         pObj.waitUtilTitle(searchStr);
         assertEquals(pObj.getCurrentTitle(), searchStr+" - Google Search");
+    }
+    
+    /**
+     * Test search special characters
+     */
+    @Test
+    public void testSpecialCharacters() {
+        String searchStr = "1*2%$(";
+        pObj.search(searchStr);
+        pObj.waitUtilTitle(searchStr);
+        assertEquals(pObj.getCurrentTitle(), searchStr+" - Google Search");
+    }
+    
+    @Test
+    public void testGmailClick() {
+    	pObj.getGmailLink().click();
+    	String url = "https://www.google.com/intl/en/mail/help/about.html";
+    	pObj.waitUtilUrl(url);
+    	assertEquals(pObj.getCurrentTitle(), "Gmail - Free Storage and Email from Google");
     }
     
     @After
--- src/main/java/com/seleniumtest/PageObject.java
+++ src/main/java/com/seleniumtest/PageObject.java
@@ -25,6 +25,22 @@ public class PageObject {
 		return driver.findElement(By.name("q"));
 	}
 	
+	public WebElement getSubmitBtn() {
+		return driver.findElement(By.name("btnK"));
+	}
+	
+	public WebElement getLuckyBtn() {
+		return driver.findElement(By.name("btnI"));
+	}
+	
+	public WebElement getGmailLink() {
+		return driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[1]/div[1]/a"));
+	}
+	
+	public WebElement getImageLink() {
+		return driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[1]/div[2]/a"));
+	}
+	
 	public void typeInputText(String searchString) {
 		inputElement.sendKeys(searchString);
 	}
@@ -38,6 +54,14 @@ public class PageObject {
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().startsWith(title);
+            }
+        });
+	}
+	
+	public void waitUtilUrl(final String url) {
+        (new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getCurrentUrl().startsWith(url);
             }
         });
 	}
